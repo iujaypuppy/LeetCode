4896941f8973d40214055d02c0bd19ac187e7b70
Zhang <jie.zhang7@hpe.com>
2017-07-13 15:19:26 +0800

    Rearrange the directory structure

--- puppy/Add_Two_Numbers/add_two_numbers.cpp
+++ puppy/Add_Two_Numbers/add_two_numbers.cpp
@@ -5,37 +5,43 @@ struct ListNode {
 };
 
 class Solution {
-	public:
-		ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-			ListNode* res;
-			int increase = 0;
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode* res = new ListNode(0);
+		ListNode* rootNode = NULL;
+		int increase = 0;
 
 
-			while(l1!=NULL || l2!=NULL)
+		while (l1 != NULL || l2 != NULL || increase!=0)
+		{
+			int val1 = 0;
+			int val2 = 0;
+			int sum = 0;
+
+			if (l1 != NULL)
 			{
-				int val1 = 0;
-				int val2 = 0;
-				int sum = 0;
-
-				if (l1!=NULL)
-				{
-					val1 = l1->val;
-					l1 = l1->next;
-				}
-				if (l2!=NULL)
-				{
-					val2 = l2->val;
-					l2 = l2->next;
-				}
-
-				sum = val1 + val2 + increase;            
-				increase = sum % 10;
-				res = new ListNode(sum/10);
-
-				res = res->next;
+				val1 = l1->val;
+				l1 = l1->next;
+			}
+			if (l2 != NULL)
+			{
+				val2 = l2->val;
+				l2 = l2->next;
 			}
 
-			return res;	
+			sum = val1 + val2 + increase;
+			increase = sum / 10;
+			res->next = new ListNode(sum % 10);
+
+			if (rootNode == NULL)
+			{
+				rootNode = res->next;
+			}
+
+			res = res->next;
 		}
 
+		return rootNode;
+
+	}
 };

41c73e7c4f587425aa1e26265a70ce697e71c7f3
plusun <tomsun.0.7@gmail.com>
2017-07-17 14:46:03 +0800

    007

---
+++ zyy/007.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int reverse(int x) {
+        int unsignedVal = x > 0 ? x : -x;
+        int sign = x > 0 ? 1 : -1;
+        int64_t reversedVal = 0;
+        /*
+          if you change this to 16, you can use this function
+          for hexadecimal, 8 for octal, etc.
+         */
+        const int base = 10;
+
+        while (unsignedVal != 0) {
+            int digit = unsignedVal % base;
+            unsignedVal /= base;
+            reversedVal = reversedVal * 10 + digit;
+        }
+
+        reversedVal *= sign;
+        int ret = (int)reversedVal;
+        /* check for overflowing */
+        if (ret != reversedVal) {
+            return 0;
+        }
+
+        return ret;
+    }
+};

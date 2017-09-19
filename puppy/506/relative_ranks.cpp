e3ba4f88de11abc1ae9704c31bb1243b7f71a17f
张杰 <zhangjie@Cindys-MacBook-Air.local>
2017-09-19 22:19:16 +0800

    506: Reversion--Relative ranks

--- puppy/506/relative_ranks.cpp
+++ puppy/506/relative_ranks.cpp
@@ -2,18 +2,29 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& nums) {
         vector<string> res;
-        vector<int> sort_nums(nums);
-        sort(sort_nums.begin(), sort_nums.end());
         for (vector<int>::iterator i(nums.begin()); i != nums.end(); i++) {
-            if (*i == *(sort_nums.end() - 1)) {
-                res.push_back("Gold Medal");
-            } else if (*i == *(sort_nums.end() - 1)) {
-                res.push_back("Gold Medal");
-            } else if (*i == *(sort_nums.end() - 1)) {
-                res.push_back("Gold Medal");
-            } else {
-                res.push_back(to_string(i - nums)
+            int rank = 1;
+            for (vector<int>::iterator j(nums.begin()); j != nums.end(); j++) {
+                if (*j > *i) {
+                    rank++;
+                }
+            }
+            switch (rank) {
+                case 1 :
+                    res.push_back("Gold Medal");
+                    break;
+                case 2 :
+                    res.push_back("Silver Medal");
+                    break;
+                case 3 :
+                    res.push_back("Bronze Medal");
+                    break;
+                default :
+                    res.push_back(to_string(rank));
+                    break;
+                    
             }
         }
+        return res;
     }
 };

c1fe778c331eff102ff12bdc1912c7dbdbfa745c
plusun <tomsun.0.7@gmail.com>
2017-09-25 08:42:00 +0800

    088

---
+++ zyy/088.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i1(m - 1), i2(n - 1), k(m + n - 1);
+        while (i1 >= 0 || i2 >= 0) {
+            if (i1 >= 0 && (i2 < 0 || nums1[i1] >= nums2[i2])) {
+                nums1[k--] = nums1[i1--];
+                continue;
+            }
+            nums1[k--] = nums2[i2--];
+        }
+    }
+};

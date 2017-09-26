bce7336c93b992ae489f1d7bef0fc7443f73b1c2
plusun <tomsun.0.7@gmail.com>
2017-09-26 15:50:16 +0800

    535

--- zyy/535.cpp
+++ zyy/535.cpp
@@ -1,5 +1,15 @@
 class Solution {
   public:
+    /*
+      This is an open question, so I do not code it seriously.
+      The points are two:
+        1. Hash from longUrl to shortUrl
+        2. State for convert shortUrl back
+      The first point aims to use shortest url and the second
+      aims to restore original url. I don't know how to use stateless
+      method and I think it is not possible to shorten url with stateless
+      method perfectly.
+     */
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
         return longUrl;

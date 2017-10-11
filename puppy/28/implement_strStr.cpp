620bac8ee62be27ab645310b9912e44656148325
Cindy Zhang <iujay1122@126.com>
2017-10-11 22:09:53 +0800

    28: Implement strStr

--- puppy/28/implement_strStr.cpp
+++ puppy/28/implement_strStr.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int found = haystack.find(needle);
-        if (found == string::npos) {
-            found = -1;
+        size_t lenHay(haystack.size()), lenNed(needle.size());
+        if (lenHay < lenNed) {
+            return -1;
         }
-        return found;
+        if (lenHay == lenNed && lenHay == 0) {
+            return 0;
+        }
+        
+        for (size_t i(0); i < lenHay; i++) {
+            if (i + lenNed > lenHay) {
+                return -1;
+            }
+            bool found = true;
+            for (size_t j(0); j < lenNed; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return i;
+            }
+        }
+        return -1;
     }
 };
